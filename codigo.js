30d72ba4359048eb064c3ffe9464dadc286f6bb2
EzequielMoreira <eezequielmoreira@gmail.com>
2025-01-05 21:35:34 -0300

    add codigo.js

--- codigo.js
+++ codigo.js
@@ -5,3 +5,17 @@
 
 let frutas = ["Ezequiel", "Pedro", "Jose", "Manzana", "Casa", "Auto"];
 document.write(frutas)
+
+
+document.write("<br><br>"); // Salto de linea para separar las secciones/ejemplos.
+
+
+// Operadores en Javascript, con ellos podemos acceder a un elemento en particular del array respetando su posición.
+
+let Operadores = [1,2,3,4]
+document.write("El tercer elemento del array es " + Operadores[2]);
+
+
+document.write("<br><br>");
+
+
