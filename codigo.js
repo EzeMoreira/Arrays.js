d8de9f697e80c46ff8ce420c337c70d7bd316be0
EzequielMoreira <eezequielmoreira@gmail.com>
2025-01-05 21:13:56 -0300

    add codigo js

--- codigo.js
+++ codigo.js
@@ -1,1 +1,7 @@
-// Ejemplos de Arrays con su respectiva documentación
+                            // Ejemplos de Arrays con su respectiva documentación
+
+
+// Array simple de ejemplo
+
+let frutas = ["Ezequiel", "Pedro", "Jose", "Manzana", "Casa", "Auto"];
+document.write(frutas)
