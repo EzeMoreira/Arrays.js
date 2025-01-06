8a6a21371244d0f37bbeb99483e54f087891d72c
EzequielMoreira <eezequielmoreira@gmail.com>
2025-01-05 21:57:26 -0300

    add js

--- codigo.js
+++ codigo.js
@@ -1,7 +1,7 @@
                             // Ejemplos de Arrays con su respectiva documentación
 
 
-// Array simple de ejemplo
+        // Array simple de ejemplo
 
 let frutas = ["Ezequiel", "Pedro", "Jose", "Manzana", "Casa", "Auto"];
 document.write(frutas)
@@ -19,3 +19,30 @@ document.write("El tercer elemento del array es " + Operadores[2]);
 document.write("<br><br>");
 
 
+        // Acceso a elementos del Array
+
+// Para conocer el número de elementos que tiene un array utilizamos la propiedad " .length " que nos
+// devuelve el número de elementos existentes en un array.
+
+const autos = ["Audi", "BMW", "Mercedes Benz", "Fiat", "Renault", "KIA"];
+const marcaAutos = autos.length
+document.write("La cantidad de marcas de autos son ", marcaAutos);
+
+
+document.write("<br><br>");
+
+
+        // Metodos para utilizar en los Arrays
+
+// Añadir o eliminar 
+// .unshift() y .shift() son métodos que actúan al inicio del array.
+
+const elements = ["a", "b", "c", "d", "e"]
+elements.unshift("XYZ"); //Agrega uno o mas elementos y devuelve una nueva longitud del array
+document.write(elements)
+
+document.write("<br><br>");
+
+const elementos = ["Jorge", "Matias", "Lucas"]
+elementos.shift(); //elimina el primer elemento de un array y devuelve una nueva longitud del array
+document.write(elementos)
